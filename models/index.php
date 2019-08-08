f4d4b41a5b097f0dc0d7fc220e1f43b411aa755b
unknown <Administrator@asvnpnpc72.pn.com.vn>
2019-08-08 16:19:00 +0700

    connect db in model

---
+++ models/index.php
@@ -0,0 +1,23 @@
+<?php 
+/**
+ * 
+ */
+class Database
+{
+	public function connect()
+	{
+		$servername = "localhost";
+		$username = "username";
+		$password = "password";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password);
+
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		} 
+		echo "Connected successfully";
+		}
+}
+ ?>
