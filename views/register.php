eff8a2549acaf777851b61944c35e134106329a6
unknown <Administrator@asvnpnpc32.pn.com.vn>
2019-08-09 08:56:43 +0700

    đã hoàn thành register.php

---
+++ views/register.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Trang đăng nhập</title>
+</head>
+<body>
+	<form action="#" method="POST" role="form">
+		<legend>LOGIN FORM</legend>
+	
+		<div class="form-group">
+			<label for="">username</label>
+			<input type="text" class="form-control" id="" placeholder="Input field">
+		</div>
+		<div class="form-group">
+			<label for="">password</label>
+			<input type="text" class="form-control" id="" placeholder="Input field">
+		</div>
+		<div>
+		<button type="submit" class="btn btn-primary">Login</button>
+		</div>
+	</form>
+</body>
+</html>
