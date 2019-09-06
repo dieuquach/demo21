fb77e1eeccd7f13d2fa72a02454e7f4d62f58786
Ho Van Huy <huy.ho@pn.com.vn>
2019-09-06 16:20:05 +0700

    Đã làm xong layout trang web

--- views/index.php
+++ views/index.php
@@ -5,5 +5,16 @@
 </head>
 <body>
 	<center><h1>Đây là trang chủ</h1></center>
+	<div class="container">
+		<div>
+			<p>Đây là header</p>
+		</div>
+		<div>
+			<p>Đây là nội dung trang web</p>
+		</div>
+		<div>
+			<p>Đây là footer</p>
+		</div>
+	</div>
 </body>
 </html>
